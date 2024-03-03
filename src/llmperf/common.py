944cb59dcad3423cc3766a9e37fbdff3c4c52791
Chirag Jain <jain.chirag925@gmail.com>
2024-03-03 19:39:58 +0530

    Revert "Removed unused code"
    This reverts commit f4c5720f012073a9652df968a4cab1bfd45f570f.

--- src/llmperf/common.py
+++ src/llmperf/common.py
@@ -1,8 +1,10 @@
 from typing import List
+from llmperf.ray_clients.litellm_client import LiteLLMClient
 from llmperf.ray_clients.openai_chat_completions_client import (
     OpenAIChatCompletionsClient,
 )
 from llmperf.ray_clients.sagemaker_client import SageMakerClient
+from llmperf.ray_clients.vertexai_client import VertexAIClient
 from llmperf.ray_llm_client import LLMClient
 
 
@@ -24,6 +26,10 @@ def construct_clients(llm_api: str, num_clients: int) -> List[LLMClient]:
         clients = [OpenAIChatCompletionsClient.remote() for _ in range(num_clients)]
     elif llm_api == "sagemaker":
         clients = [SageMakerClient.remote() for _ in range(num_clients)]
+    elif llm_api == "vertexai":
+        clients = [VertexAIClient.remote() for _ in range(num_clients)]
+    elif llm_api in SUPPORTED_APIS:
+        clients = [LiteLLMClient.remote() for _ in range(num_clients)]
     else:
         raise ValueError(
             f"llm_api must be one of the supported LLM APIs: {SUPPORTED_APIS}"
